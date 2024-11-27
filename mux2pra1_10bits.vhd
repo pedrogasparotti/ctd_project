4295920e9bff6d8c051858aa2ba458a91e3089e9
pedrovinicius <home@eletrogralha.local>
2024-11-27 18:28:27 -0300

    last stable version

--- mux2pra1_10bits.vhd
+++ mux2pra1_10bits.vhd
@@ -1,14 +1,22 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-entity mux2pra1_10bits is
-    Port ( sel : in STD_LOGIC;
-           x,y : in STD_LOGIC_VECTOR(9 downto 0);
-           saida : out STD_LOGIC_VECTOR(9 downto 0));
-end mux2pra1_10bits;
+entity reg10bits is
+    Port ( CLK, RST, enable : in STD_LOGIC;
+           D : in STD_LOGIC_VECTOR(9 downto 0);
+           Q : out STD_LOGIC_VECTOR(9 downto 0));
+end reg10bits;
 
-architecture circuito of mux2pra1_10bits is
+architecture behv of reg10bits is
 begin
-    saida <= x when sel = '0' else
-             y ;
-end circuito;
+    process(CLK, RST)
+    begin
+        if RST = '1' then
+            Q <= "0000000000";
+        elsif rising_edge(CLK) then
+            if enable = '1' then
+                Q <= D;
+            end if;
+        end if;
+    end process;
+end behv;
